199c12f80e71badd326f109d64f61c27bbd6b2c7
Brian Vallelunga <vallelungabrian@gmail.com>
2014-07-12 08:47:35 +0000

    updated manifest.json

---
+++ index.js
@@ -0,0 +1,99 @@
+/* Compiled by kdc on Sat Jul 12 2014 08:47:17 GMT+0000 (UTC) */
+(function() {
+/* KDAPP STARTS */
+/* BLOCK STARTS: /home/bvallelunga/Applications/KDApp/apps/Preview.kdapp/index.coffee */
+var PreviewController, PreviewMainView,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+PreviewMainView = (function(_super) {
+  __extends(PreviewMainView, _super);
+
+  function PreviewMainView(options, data) {
+    if (options == null) {
+      options = {};
+    }
+    options.cssClass = 'preview main-view';
+    this.user = KD.nick();
+    this.port = this.getParameterByName("port");
+    PreviewMainView.__super__.constructor.call(this, options, data);
+  }
+
+  PreviewMainView.prototype.viewAppended = function() {
+    if (this.port) {
+      return KodingAppsController.appendHeadElements({
+        identifier: "preview",
+        items: [
+          {
+            type: 'style',
+            url: "http://" + this.user + ".kd.io:" + this.port + "/style.css"
+          }, {
+            type: 'script',
+            url: "http://" + this.user + ".kd.io:" + this.port + "/index.js"
+          }
+        ]
+      }, console.log);
+    } else {
+      return this.addSubView(this.alert = new KDCustomHTMLView({
+        tagName: "div",
+        cssClass: "alert",
+        partial: "Please specify a port to listen to..."
+      }));
+    }
+  };
+
+  PreviewMainView.prototype.getParameterByName = function(name) {
+    var regex, results;
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    results = regex.exec(location.search);
+    if (results) {
+      return decodeURIComponent(results[1].replace(/\+/g, ""));
+    } else {
+      return "";
+    }
+  };
+
+  return PreviewMainView;
+
+})(KDView);
+
+PreviewController = (function(_super) {
+  __extends(PreviewController, _super);
+
+  function PreviewController(options, data) {
+    if (options == null) {
+      options = {};
+    }
+    options.view = new PreviewMainView;
+    options.appInfo = {
+      name: "Preview",
+      type: "application"
+    };
+    PreviewController.__super__.constructor.call(this, options, data);
+  }
+
+  return PreviewController;
+
+})(AppController);
+
+(function() {
+  var view;
+  if (typeof appView !== "undefined" && appView !== null) {
+    view = new PreviewMainView;
+    return appView.addSubView(view);
+  } else {
+    return KD.registerAppClass(PreviewController, {
+      name: "Preview",
+      routes: {
+        "/:name?/Preview": null,
+        "/:name?/bvallelunga/Apps/Preview": null
+      },
+      dockPath: "/bvallelunga/Apps/Preview",
+      behavior: "application"
+    });
+  }
+})();
+
+/* KDAPP ENDS */
+}).call();
